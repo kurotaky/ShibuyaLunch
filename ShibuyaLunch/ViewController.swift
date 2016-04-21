34fca1be05a8fb58dde36552a521469454241957
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-04-21 09:49:54 +0900

    initial commit

---
+++ ShibuyaLunch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShibuyaLunch
+//
+//  Created by usr0600244 on 2016/04/21.
+//  Copyright © 2016年 org.mo-fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
